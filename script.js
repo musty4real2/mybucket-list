4cbf79deb2308b73ffbbc558a344f2dfd6eddedf
musty4real2 <musty4real2@gmail.com>
2024-02-07 14:23:17 +0100

    server

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const inputFieldEl = document.getElementById("input-field")
-const addToCartBtn=document.getElementById('add-button')
 
-addToCartBtn.addEventListener("click", function(){ 
-    let inputValue = inputFieldEl.value
-    // console.log("Hey am clicked!!!")
-    console.log(inputValue)
-})
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+    import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+    
+    const appSettings = {
+        databaseURL: "https://playground-afa40-default-rtdb.europe-west1.firebasedatabase.app/"
+    }
+    
+    const app = initializeApp(appSettings)
+    const database = getDatabase(app)
+    const moviesInDB = ref(database, "movies")
+    
+    const inputFieldEl = document.getElementById("input-field")
+    const addButtonEl = document.getElementById("add-button")
+    
+    addButtonEl.addEventListener("click", function() {
+        let inputValue = inputFieldEl.value
+        
+        push(moviesInDB, inputValue)
+        
+        console.log(`${inputValue} added to database`)
+    })
