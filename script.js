984793cc278a816842f45af9d0f1f0ff4e6897bc
musty4real2 <musty4real2@gmail.com>
2024-02-11 00:24:13 +0100

    additions

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-    import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-afa40-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "movies")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
     
-    const appSettings = {
-        databaseURL: "https://playground-afa40-default-rtdb.europe-west1.firebasedatabase.app/"
-    }
+    push(shoppingListInDB, inputValue)
+    // appendItemToShoppingListEl(inputValue)
     
-    const app = initializeApp(appSettings)
-    const database = getDatabase(app)
-    const moviesInDB = ref(database, "movies")
+    // clearInputFieldEl()
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
     
-    const inputFieldEl = document.getElementById("input-field")
-    const addButtonEl = document.getElementById("add-button")
-    const shoppingListEl = document.getElementById("shopping-list")
+    clearShoppingListEl()
     
-    addButtonEl.addEventListener("click", function() {
-        let inputValue = inputFieldEl.value
+    for (let i = 0; i < itemsArray.length; i++) {
+        // appendItemToShoppingListEl(itemsArray[i])
+        let currentItem = itemsArray[i]
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
         
-        push(moviesInDB, inputValue)
-
-        // inputFieldEl.value = ""
-        clearInputFieldEl()
-        appendItemToShoppingListEl(inputValue)
-        // console.log(`${inputValue} added to database`)
-    })
-
-    function clearInputFieldEl() {
-        inputFieldEl.value = ""
+        appendItemToShoppingListEl(currentItemValue)
     }
+})
 
-    function appendItemToShoppingListEl(itemValue) {
-        shoppingListEl.innerHTML += `<li>${itemValue}</li>`
-    }
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
